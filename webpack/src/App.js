cde934d9ea9ac28dd56851b21b3706dac71e1754
carneiroaraujo <joaosheep95@gmail.com>
2024-05-29 19:54:42 -0300

    Add styling to notes app

---
+++ webpack/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="container">
+            hello webpack
+        </div>
+    )
+}
+
+export default App
---
+++ webpack/src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import App from "./App"
+import "./index.css"
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
+
+function hello(name) {
+    console.log(`hello ${name}`);
+}
+
+// App()
+
